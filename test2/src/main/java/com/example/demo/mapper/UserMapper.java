93188a3cf81cf476db80ee237506f2d501702746
Thewillman <1137399580@qq.com>
2020-11-13 00:01:09 +0800

    苏艺淞代码，目前可以连接网页但是不知道数据库放哪了（mybatis-generator反向生成的代码）

---
+++ test2/src/main/java/com/example/demo/mapper/UserMapper.java
@@ -0,0 +1,38 @@
+package com.example.demo.mapper;
+
+import com.example.demo.entity.User;
+import com.example.demo.entity.UserExample;
+import com.example.demo.entity.UserKey;
+import com.example.demo.entity.UserWithBLOBs;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface UserMapper {
+    long countByExample(UserExample example);
+
+    int deleteByExample(UserExample example);
+
+    int deleteByPrimaryKey(UserKey key);
+
+    int insert(UserWithBLOBs record);
+
+    int insertSelective(UserWithBLOBs record);
+
+    List<UserWithBLOBs> selectByExampleWithBLOBs(UserExample example);
+
+    List<User> selectByExample(UserExample example);
+
+    UserWithBLOBs selectByPrimaryKey(UserKey key);
+
+    int updateByExampleSelective(@Param("record") UserWithBLOBs record, @Param("example") UserExample example);
+
+    int updateByExampleWithBLOBs(@Param("record") UserWithBLOBs record, @Param("example") UserExample example);
+
+    int updateByExample(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByPrimaryKeySelective(UserWithBLOBs record);
+
+    int updateByPrimaryKeyWithBLOBs(UserWithBLOBs record);
+
+    int updateByPrimaryKey(User record);
+}
